bb3ae3574ef1ce339739e524750c4e37fc7808a8
Evortex <codex03@mail.ru>
2025-02-25 13:11:27 +0700

    обновление структуры проекта

--- src/main/java/com/example/tourist/TouristServiceApplication.java
+++ src/main/java/com/example/tourist/TouristServiceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TouristServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TouristServiceApplication.class, args);
 	}
-
 }
